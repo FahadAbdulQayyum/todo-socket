2d9c4386d16166f54234935db8e50194ac7342ea
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-05-06 01:09:53 +0500

    removed unused stuffs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,13 @@
 "use client"
 
-// import FlashSale from '@/components/FlashSale'
-// import FirstLook from '@/components/FirstLook'
-// import Hero from '@/components/Hero'
-// import PRIVApp from '@/components/PRIVApp'
 import React from 'react'
-// import Featured from '@/components/Featured'
-// import GearUp from '@/components/Gearup'
-// import DontMiss from '@/components/DontMiss'
-// import Essential from '@/components/Essential'
-// import CategoryList from '@/components/CategoryList'
 import Admin from '@/components/Admin'
-import UseWebSocket from '@/components/WebSocket'
-
 
 const page = () => {
-
-  const data = UseWebSocket('ws://localhost:8080')
   
   return (
     <div>
       <Admin />
-      <h1>Live Updates</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      {/* <PRIVApp />
-      <Hero />
-      <FirstLook />
-      <FlashSale />
-      <Featured />
-      <GearUp />
-      <DontMiss />
-      <Essential />
-      <CategoryList /> */}
     </div >
   )
 }
