763572c44807457b48bcbd7fbbc0ad884aa3f921
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-05-06 02:00:36 +0500

    Plus icon added with edit and delete button

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -2,11 +2,14 @@
 
 import { setLoading } from '@/components/lib/features/loader/loaderSlice';
 import { useAppDispatch } from '@/components/lib/useAppDispatch';
+import { MinusCircle, PlusCircle } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 export default function TasksPage() {
   const [tasks, setTasks] = useState<any[]>([]);
   const dispatch = useAppDispatch();
+  const router = useRouter()
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -32,13 +35,24 @@ export default function TasksPage() {
       <h1 className="text-2xl font-bold mb-4">Your Tasks</h1>
       <ul>
         {tasks.map((task) => (
-          <li key={task.taskId} className="border-b py-2">
-            <h3 className="font-semibold">{task.title}</h3>
-            <p>{task.description}</p>
-            <p className="text-sm text-gray-500">{task.completed ? 'Completed' : 'Incomplete'}</p>
+          <li key={task.taskId} className="flex justify-between border-b py-2 pe-2">
+            <span>
+              <h3 className="font-semibold">{task.title}</h3>
+              <p>{task.description}</p>
+              <p className="text-sm text-gray-500">{task.completed ? 'Completed ✅' : 'Incomplete ❌'}</p>
+            </span>
+            <span className='flex flex-col space-y-2'>
+              {/* <button className='bg-red-500 p-2 text-white rounded-lg'>Delete</button>
+              <button className='bg-green-500 p-2 text-white rounded-lg'>Edit</button> */}
+              <button className='border bg-black p-2 text-white rounded-lg'>Delete</button>
+              <button className='border border-black bg-white p-2 text-black rounded-lg'>Edit</button>
+            </span>
           </li>
         ))}
       </ul>
-    </div>
+      <div className='flex justify-center space-x-4 w-full mt-4'>
+        <PlusCircle className="text-6xl" aria-label="Favorites" size={40} onClick={() => router.push('tasks/create')} />
+      </div>
+      </div>
   );
 }
