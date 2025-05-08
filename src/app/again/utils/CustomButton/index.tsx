fc8a561e0bde12f051249be36faf7759c9dd0327
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-05-08 15:08:39 +0500

    Removed commented part of code

--- src/app/again/utils/CustomButton/index.tsx
+++ src/app/again/utils/CustomButton/index.tsx
@@ -4,7 +4,6 @@ const CustomButton = ({children}: any) => {
   return (
     <button className="flex items-center space-x-2 bg-black text-white p-4 !font-light rounded-md">
         {children}
-        {/* <MoveRight size={15} /> */}
     </button>
   )
 }
